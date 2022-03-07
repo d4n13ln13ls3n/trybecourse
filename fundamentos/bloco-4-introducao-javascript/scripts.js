0860e946d442910e0a1517604504f93c348b2051
Daniel Yabu <danielyabu@yahoo.com>
2022-03-07 15:24:21 -0300

    Atualizados arquivos index.html, styles.css, index2.html e styles2.css  do bloco 3.4

---
+++ fundamentos/bloco-4-introducao-javascript/scripts.js
@@ -0,0 +1,18 @@
+let estatus = "70";
+
+switch (estatus) {
+  case "70":
+  console.log("aprovação");
+  break;
+
+  case "60":
+  console.log("lista");
+  break;
+
+  case "50":
+  console.log("reprovação");
+  break;
+
+  default:
+    console.log("não se aplica");
+}
