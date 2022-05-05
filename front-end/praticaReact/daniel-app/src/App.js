9c26470a884847e11fd5dbff2e5d3cea7aede0bd
Daniel Yabu <danielyabu@yahoo.com>
2022-05-05 15:32:49 -0300

    Comitando arquivos pendentes

---
+++ front-end/praticaReact/daniel-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Whatever from './Component';
+
+function App() {
+  return (<Whatever />);
+}
+
+export default App;
---
+++ front-end/praticaReact/daniel-app/src/Component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Whatever extends React.Component {
+  render () {
+    return (<div>
+      <h1>My name is Daniel</h1>
+      <p>This is my second attempt at making components appear on the screen</p>
+    </div>
+    )
+  }
+}
+
+export default Whatever;
